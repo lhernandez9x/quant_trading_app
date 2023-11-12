872c39d6369447c738b9bcfc0b2b5dcb6290429c
Lorenzo Hernandez <lorenzo.hernandezjr@gmail.com>
2023-11-12 12:32:45 -0700

    ADD: created models for stocks and prices

--- data_retrieval/models.py
+++ data_retrieval/models.py
@@ -4,3 +4,18 @@ from django.db import models
 
 class Symbol(models.Model):
     ticker = models.CharField(max_length=200)
+
+    class Meta():
+        verbose_name_plural = 'symbol'
+
+class Prices(models.Model):
+    ticker = models.ForeignKey(Symbol, on_delete=models.CASCADE)
+    trade_date = models.DateField()
+    open_price = models.DecimalField(max_digits=20, decimal_places=2)
+    high_price = models.DecimalField(max_digits=20, decimal_places=2)
+    low_price = models.DecimalField(max_digits=20, decimal_places=2)
+    close_price = models.DecimalField(max_digits=20, decimal_places=2)
+    volume = models.IntegerField()
+
+    class Meta:
+        verbose_name_plural = 'prices'
